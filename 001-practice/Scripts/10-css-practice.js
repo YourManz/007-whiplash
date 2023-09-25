daa058e52796210ad67e11b22dc3b940c795e69f
YourManz <kobeneilson@gmail.com>
2023-09-25 13:59:24 -0700

    CSS, JS and HTML practice
    I created a basic page that has three buttons that visually toggle. Those buttons toggled state is stored in localStorage and are independant from eachother.

---
+++ 001-practice/Scripts/10-css-practice.js
@@ -0,0 +1,27 @@
+const toggled = JSON.parse(localStorage.getItem('toggled')) || {
+  gaming:false,
+  music:false,
+  tech:false
+};
+
+
+if (toggled.gaming === true) {
+  document.querySelector(`.js-gaming-button`).classList.add('toggled-button');
+} 
+if (toggled.music === true) {
+  document.querySelector(`.js-music-button`).classList.add('toggled-button');
+}
+if (toggled.tech === true) {
+  document.querySelector(`.js-tech-button`).classList.add('toggled-button');
+}
+
+function toggleButton(a) {
+  const buttonClass = document.querySelector(`.js-${a}-button`);
+  toggled[a] = (!toggled[a]);
+  if (toggled[a] === true) {
+    buttonClass.classList.add('toggled-button');
+  } else {
+    buttonClass.classList.remove('toggled-button');
+  }
+localStorage.setItem('toggled', JSON.stringify(toggled))
+}
