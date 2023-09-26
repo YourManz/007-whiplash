63e9e73d2c535a1a299c8c83e9bf5f38bcf51fe7
YourManz <kobeneilson@gmail.com>
2023-09-26 13:12:37 -0700

    New projects structure

--- 001-practice/Scripts/10-css-practice.js
+++ 001-practice/Scripts/10-css-practice.js
@@ -4,24 +4,25 @@ const toggled = JSON.parse(localStorage.getItem('toggled')) || {
   tech:false
 };
 
-
 if (toggled.gaming === true) {
   document.querySelector(`.js-gaming-button`).classList.add('toggled-button');
-} 
+};
 if (toggled.music === true) {
   document.querySelector(`.js-music-button`).classList.add('toggled-button');
-}
+};
 if (toggled.tech === true) {
   document.querySelector(`.js-tech-button`).classList.add('toggled-button');
-}
+};
 
 function toggleButton(a) {
   const buttonClass = document.querySelector(`.js-${a}-button`);
+  //document.querySelector('.category-button').classList.remove('toggled-button');
   toggled[a] = (!toggled[a]);
+  
   if (toggled[a] === true) {
     buttonClass.classList.add('toggled-button');
   } else {
     buttonClass.classList.remove('toggled-button');
   }
 localStorage.setItem('toggled', JSON.stringify(toggled))
-}
+};
