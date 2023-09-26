63205049e59e0b4999b3ae47fe7560865b66b9cb
YourManz <kobeneilson@gmail.com>
2023-09-26 14:10:29 -0700

    Started working on loops, may need more assistance then I wouold like

--- 001-practice/Scripts/11-arrays-and-loops.js
+++ 001-practice/Scripts/11-arrays-and-loops.js
@@ -14,3 +14,36 @@ function clearList() {
   todoOne.splice(0, listLngth);
   localStorage.setItem('todoOne', JSON.stringify(todoOne));
 }
+//==========================================================================
+const todoTwo= JSON.parse(localStorage.getItem('todoTwo')) || [];
+function addItem2() {
+  const todoInput = document.querySelector('.js-input-2');
+  const listLngth = todoTwo.length;
+
+  todoTwo.push(todoInput.value);
+  localStorage.setItem('todoOne', JSON.stringify(todoTwo));
+  document.querySelector('.js-input-2').value = '';
+  document.querySelector('.js-todo-list-two').innerHTML = ``;
+  
+  todoTest = [
+    'one',
+    'two',
+    'three'
+  ]
+  
+  for (index = 0; todoTest.length - 1; index++) {
+    document.querySelector('.js-todo-list-two').innerHTML = `${todoTest[index]}\n`;
+    console.log(todoTest[index]);
+  }
+  
+}
+
+function consoleLogArray() {
+  console.log(todoOne);
+}
+
+function clearList() {
+  const listLngth = todoOne.length;
+  todoOne.splice(0, listLngth);
+  localStorage.setItem('todoOne', JSON.stringify(todoOne));
+}
