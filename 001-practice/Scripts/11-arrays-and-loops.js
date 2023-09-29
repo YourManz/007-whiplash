0b3efff8749f86312175bba665dce46acef9104d
YourManz <kobeneilson@gmail.com>
2023-09-28 22:15:32 -0700

    Small feature
    I added a feature in which the clear button will also clear the input boxes

--- 001-practice/Scripts/11-arrays-and-loops.js
+++ 001-practice/Scripts/11-arrays-and-loops.js
@@ -3,17 +3,17 @@ function addItem1() {
   todoOne.push(document.querySelector('.js-input-1').value);
   localStorage.setItem('todoOne', JSON.stringify(todoOne));
   document.querySelector('.js-input-1').value = '';
-}
+};
 
 function consoleLogArray() {
   console.log(todoOne);
-}
+};
 
 function clearList() {
   const listLngth = todoOne.length;
   todoOne.splice(0, listLngth);
   localStorage.setItem('todoOne', JSON.stringify(todoOne));
-}
+};
 //==========================================================================
 const todoTwo= JSON.parse(localStorage.getItem('todoTwo')) || [];
 function addItem2() {
@@ -28,7 +28,7 @@ function addItem2() {
   document.querySelector('.js-todo-list-two').innerHTML = ``;
 
   addSecondList();
-}
+};
 
 function addSecondList() {
   for (index = 0; index <= todoTwo.length-1; index++) {
@@ -36,7 +36,7 @@ function addSecondList() {
     document.querySelector('.js-todo-list-two').innerHTML += `<div class="basic-text">${todoTwo[index].todo}</div> <div class="basic-text">${todoTwo[index].date}</div> <button onclick="deleteSingleItem(${index})" class="red-button">Delete</button> <div></div>`;
   };
 };
-addSecondList()
+addSecondList();
 
 function deleteSingleItem(i) {
   todoTwo.splice(i, 1);
@@ -50,5 +50,6 @@ function clearListTwo() {
   todoTwo.splice(0, listLngth);
   localStorage.setItem('todoTwo', JSON.stringify(todoTwo));
   document.querySelector('.js-todo-list-two').innerHTML = ``;
-}
-
+  document.querySelector('.js-input-2').value = ``;
+  document.querySelector('.js-date').value = ``;
+};
