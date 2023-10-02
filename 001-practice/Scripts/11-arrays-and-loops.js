a12b752147e9b897a10515ad26f80c38e24b1b60
YourManz <kobeneilson@gmail.com>
2023-10-02 12:39:13 -0700

    Deleted first todo list

--- 001-practice/Scripts/11-arrays-and-loops.js
+++ 001-practice/Scripts/11-arrays-and-loops.js
@@ -1,20 +1,4 @@
-/*
-const todoOne= JSON.parse(localStorage.getItem('todoOne')) || [];
-function addItem1() {
-  todoOne.push(document.querySelector('.js-input-1').value);
-  localStorage.setItem('todoOne', JSON.stringify(todoOne));
-  document.querySelector('.js-input-1').value = '';
-};
 
-function consoleLogArray() {
-  console.log(todoOne);
-};
-
-function clearList() {
-  const listLngth = todoOne.length;
-  todoOne.splice(0, listLngth);
-  localStorage.setItem('todoOne', JSON.stringify(todoOne));
-}; */
 //==========================================================================
 const todoTwo= JSON.parse(localStorage.getItem('todoTwo')) || [];
 addSecondList();
