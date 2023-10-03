9f1717c46094c3ff882468651f817a1fc357e583
YourManz <kobeneilson@gmail.com>
2023-10-03 14:41:14 -0700

    Trying to make it so that only one toggle can be active at once

--- 001-practice/Scripts/10-css-practice.js
+++ 001-practice/Scripts/10-css-practice.js
@@ -3,10 +3,14 @@ const toggled = JSON.parse(localStorage.getItem('toggled')) || {
   music:false,
   tech:false
 };
+const categories = ['gaming', 'music', 'tech'];
+let pressedButton = '';
+
 
 function toggleButton(a) {
   const buttonClass = document.querySelector(`.js-${a}-button`);
   //document.querySelector('.category-button').classList.remove('toggled-button');
+  pressedButton = `${a}`;
   toggled[a] = (!toggled[a]);
   
   if (toggled[a] === true) {
@@ -15,34 +19,36 @@ function toggleButton(a) {
     buttonClass.classList.remove('toggled-button');
   }
 localStorage.setItem('toggled', JSON.stringify(toggled))
-checkButtonToggle();
+toggledCheck(a);
 };
 
-function checkButtonToggle() {
-  if (toggled.gaming === true) {
-    toggled.music = false;
-    toggled.tech = false;
-    toggledCheck();
-  }
-  if (toggled.music === true) {
-    toggled.gaming = false;
-    toggled.tech = false;
-    toggledCheck();
-  }
-  if (toggled.tech === true) {
-    toggled.gaming = false;
-    toggled.music = false;
-    toggledCheck();
-  }
-
-function toggledCheck() {
-  if (toggled.gaming === true) {
+function toggledCheck(a) {
+  if (a === 'gaming') {
     document.querySelector(`.js-gaming-button`).classList.add('toggled-button');
+    toggled.music = false;
+    toggled.tech = false;
+    loopThrough();
   };
-  if (toggled.music === true) {
+  if (a === 'music') {
     document.querySelector(`.js-music-button`).classList.add('toggled-button');
+    toggled.gaming = false;
+    toggled.tech = false;
+    loopThrough();
   };
-  if (toggled.tech === true) {
+  if (a === 'tech') {
     document.querySelector(`.js-tech-button`).classList.add('toggled-button');
+    toggled.gaming = false;
+    toggled.music = false;
+    loopThrough();
   };
+};
+
+function loopThrough() {
+  for (i = 0; i<=2; i++) {
+    const buttonClass = document.querySelector(`.js-${categories[i]}-button`);
+    console.log(categories[i]);
+    if (toggled.categories[i] === false) {
+      buttonClass.classList.remove('toggled-button');
+    };
+  }
 };
